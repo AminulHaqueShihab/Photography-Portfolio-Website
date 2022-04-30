60480aedadba6b9886bb0ed08315e92eebc921a8
AminulHaqueShihab <61130539+AminulHaqueShihab@users.noreply.github.com>
2022-04-30 22:55:11 +0600

    updated

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -10,7 +10,7 @@ class UserTest extends \PHPUnit\Framework\TestCase {
         $Users = new \App\Users;
         $Users->setProfileInfo("Md Aminul Haque",19,"https://www.facebook.com/aminul.haque.5","aminul@gmail.com", "123456");
 
-        $result = $Users->getUsername(15);
+        $result = $Users->getUsername(19);
         $this->assertEquals("Md Aminul Haque", $result);
     }
 }
